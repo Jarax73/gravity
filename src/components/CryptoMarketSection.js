011354fe50075ff64c7dbccc039b84bf587a12fd
Jarax73 <jaraxkapila73@gmail.com>
2025-01-09 20:41:53 +0100

    Feature: add partner section

--- src/components/CryptoMarketSection.js
+++ src/components/CryptoMarketSection.js
@@ -39,6 +39,7 @@ export default function CryptoMarketSection() {
           top: -850,
           right: 0,
         }}
+        alt=""
       />
       <div
         style={{
@@ -218,6 +219,234 @@ export default function CryptoMarketSection() {
               alt="bar 2"
               style={{ width: "150px", marginTop: "-80px" }}
             />
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "100px",
+        }}
+      >
+        <div>
+          <div style={{ position: "relative" }}>
+            <img
+              src="/assets/div.png"
+              alt="div"
+              style={{ position: "absolute", left: -100, top: 0 }}
+            />
+            <img
+              src="/assets/exchange-screens-new.png"
+              alt="exchange"
+              style={{ position: "absolute", left: 0, zIndex: 1 }}
+            />
+            <img
+              src="/assets/grid.png"
+              alt="grid"
+              style={{ position: "absolute", top: 200 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{ width: "370px", display: "flex", flexDirection: "column" }}
+        >
+          <h2
+            style={{ fontSize: "36px", fontWeight: "500", textAlign: "left" }}
+          >
+            Market Making for Crypto Exchanges
+          </h2>
+          <h3 style={{ fontSize: "20px", textAlign: "left" }}>
+            Attract more traders and projects with deep order books & liquidity
+          </h3>
+          <p style={{ textAlign: "left" }}>
+            Our world-class market making services are proven to help local and
+            emerging exchanges win traders and gain market-leading positions of
+            up to 90% market dominance.
+          </p>
+          <div>
+            <Link href="#">
+              Learn more
+              <img
+                src="/assets/a_after.png"
+                alt="after"
+                style={{ marginLeft: "10px" }}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ marginTop: "300px", display: "flex", height: "750px" }}>
+        <div
+          style={{
+            width: "100%",
+            gap: 300,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+          }}
+        >
+          <div style={{ position: "relative" /*border: "1px solid white"*/ }}>
+            <img
+              src="/assets/middle-left-2.png"
+              alt=""
+              style={{
+                position: "absolute",
+                left: -550,
+                top: -150,
+              }}
+            />
+            <div>
+              <img
+                src="/assets/near.png"
+                alt=""
+                style={{
+                  // width: "73px",
+                  position: "absolute",
+                  left: -30,
+                  top: 70,
+                }}
+              />
+              <img
+                src="/assets/solana.png"
+                alt=""
+                style={{
+                  // width: "96px",
+                  position: "absolute",
+                  left: -80,
+                  top: 200,
+                }}
+              />
+              <img
+                src="/assets/mena.png"
+                alt=""
+                style={{
+                  // width: "72px",
+                  position: "absolute",
+                  left: 65,
+                  top: 150,
+                }}
+              />
+            </div>
+          </div>
+          <div style={{ width: "400px" }}>
+            <h1
+              style={{ fontWeight: "500", fontSize: "60px", textAlign: "left" }}
+            >
+              Our Partners & Friends
+            </h1>
+          </div>
+        </div>
+        <div style={{ display: "flex", gap: 20 }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 25,
+              alignSelf: "flex-start",
+            }}
+          >
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/bithumb@2x.png.png" alt="" style={{}} />
+            </div>
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/bitbank@2x.png.png" alt="" style={{}} />
+            </div>
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/binance@2x.png.png" alt="" style={{}} />
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 25,
+              alignSelf: "flex-end",
+            }}
+          >
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/bitstamp@2x.png.png" alt="" style={{}} />
+            </div>
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/coinbase@2x.png.png" alt="" style={{}} />
+            </div>
+            <div
+              style={{
+                width: "190px",
+                height: "190px",
+                border: "1px solid #415572",
+                background:
+                  "linear-gradient(to bottom, rgba(65, 85, 114, 0.15), rgba(27, 27, 30, 0.10))",
+
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src="/assets/bitfinex@2x.png.png" alt="" style={{}} />
+            </div>
           </div>
         </div>
       </div>
