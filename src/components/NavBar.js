09fe329e19bb2db20366b2a4002fe06cb8eee582
Jarax73 <jaraxkapila73@gmail.com>
2025-01-11 12:46:30 +0100

    Feature: adjusting media querie for the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Nav = styled.div`
@@ -6,9 +6,41 @@ const Nav = styled.div`
   color: white;
   padding-left: 94px;
   padding-right: 94px;
-  // justify-content: space-between;
   align-items: center;
   height: 120px;
+
+  @media (max-width: 1023px) {
+    padding-left: 40px;
+    padding-right: 40px;
+    align-items: center;
+    height: 80px;
+  }
+
+  @media (max-width: 767px) {
+    padding-left: 10px;
+    padding-right: 10px;
+    align-items: center;
+    height: 80px;
+    position: relative;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+
+  @media (max-width: 1023px) {
+    flex: 2;
+  }
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const Link = styled.a`
@@ -19,7 +51,55 @@ const Link = styled.a`
   &:hover {
     color: #577bc1;
   }
+
+  @media (max-width: 1023px) {
+    font-size: 12px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 10px;
+  }
 `;
+
+const Logo = styled.img`
+  width: 30%;
+
+  @media (max-width: 1023px) {
+    width: 50%;
+  }
+
+  @media (max-width: 767px) {
+    width: 100px;
+  }
+`;
+
+const Navigation = styled.div`
+  flex: 2;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  @media (max-width: 1023px) {
+    flex: 3;
+  }
+
+  @media (max-width: 767px) {
+    height: 200%;
+    align-items: space-around;
+    justify-content: center;
+    flex-direction: column;
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding-top: 60px;
+    padding-right: 50px;
+    padding-left: 50px;
+    padding-bottom: 60px;
+    background-color: #141619;
+    box-shadow: 2px 15px 35px 15px black;
+  }
+`;
+
 const Button = styled.button`
   display: flex;
   flex: 1;
@@ -29,12 +109,45 @@ const Button = styled.button`
   align-items: center;
   background-color: #141619;
   color: white;
-  width: 150px;
   height: 50px;
   font-size: 15px;
+
+  @media (max-width: 1023px) {
+    font-size: 12px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 10px;
+    justify-content: flex-start;
+  }
+`;
+
+const BtnImg = styled.img`
+  margin-right: 10px;
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+
+  @media (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 10;
+    height: 20px;
+    // position: absolute;
+    // top: 0;
+    // right: 0;
+    padding-right: 10px;
+  }
 `;
 
 export default function NavBar() {
+  const [active, setActive] = useState(false);
   return (
     <Nav>
       <p
@@ -44,40 +157,61 @@ export default function NavBar() {
           justifyContent: "flex-start",
         }}
       >
-        <img src="/logo-light.png" alt="gravity logo" />
+        <Logo src="/logo-light.png" alt="gravity logo" />
       </p>
-      <ul
-        style={{
-          listStyle: "none",
-          margin: 0,
-          padding: 0,
-          flex: 1,
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
-        <li>
-          <Link href="#">ABOUT US</Link>
-        </li>
-        <li>
-          <Link href="#">OUR SERVICES</Link>
-        </li>
-        <li>
-          <Link href="#">WORK WITH US</Link>
-        </li>
-        <li>
-          <Link href="#">BLOG</Link>
-        </li>
-      </ul>
 
-      <Button>
-        <img
-          style={{ marginRight: "10px" }}
-          src="/button_before.png"
-          alt="button"
-        />
-        GET IN TOUCH
-      </Button>
+      {!active && window.innerWidth < 768 ? (
+        <div>
+          <Hamburger onClick={() => setActive(true)}>
+            <div style={{ border: "1px solid #bcbcbc", width: "30px" }}></div>
+            <div style={{ border: "1px solid #bcbcbc", width: "30px" }}></div>
+            <div style={{ border: "1px solid #bcbcbc", width: "30px" }}></div>
+          </Hamburger>
+        </div>
+      ) : (
+        <>
+          {window.innerWidth < 768 && (
+            <div
+              style={{
+                width: "30px",
+                fontSize: "20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                border: "1px solid white",
+                zIndex: 1,
+                position: "absolute",
+                top: 25,
+                right: 10,
+              }}
+              onClick={() => setActive(false)}
+            >
+              X
+            </div>
+          )}
+          <Navigation>
+            <List>
+              <li>
+                <Link href="#">ABOUT US</Link>
+              </li>
+              <li>
+                <Link href="#">OUR SERVICES</Link>
+              </li>
+              <li>
+                <Link href="#">WORK WITH US</Link>
+              </li>
+              <li>
+                <Link href="#">BLOG</Link>
+              </li>
+            </List>
+
+            <Button>
+              <BtnImg src="/button_before.png" alt="button" />
+              GET IN TOUCH
+            </Button>
+          </Navigation>
+        </>
+      )}
     </Nav>
   );
 }
