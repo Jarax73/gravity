32454f6a2777e6615ccd62ba74c6caffdfc3bf26
Jarax73 <jaraxkapila73@gmail.com>
2025-01-15 10:24:38 +0100

    Feature: add media queries to Partner section entirely

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -18,63 +18,132 @@ const Section = styled.section`
   }
 `;
 
+const H1 = styled.h1`
+  font-weight: 500;
+  font-size: 58px;
+
+  @media (max-width: 1023px) {
+    font-size: 50px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 36px;
+  }
+`;
+
+const Paragraph = styled.p`
+  width: 600px;
+  font-size: 20px;
+  color: #e5e5e5;
+
+  @media (max-width: 1023px) {
+    font-size: 18px;
+  }
+
+  @media (max-width: 767px) {
+    width: 90%;
+    font-size: 16px;
+  }
+`;
+
+const ArrowContainer = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: space-around;
+
+  @media (max-width: 767px) {
+    width: 30%;
+    & img {
+      width: 40px;
+    }
+  }
+`;
+
+const Comment = styled.p`
+  width: 678px;
+  font-size: 26px;
+  color: #e5e5e5 @media (max-width: 1023px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 767px) {
+    width: 90%;
+    font-size: 18px;
+  }
+`;
+
+const Name = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #e5e5e5;
+
+  @media (max-width: 1023px) {
+    font-size: 16px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 14px;
+  }
+`;
+
+const Grade = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 14px;
+
+  @media (max-width: 767px) {
+    & img {
+      width: 85px;
+    }
+  }
+`;
+
+const Dots = styled.div`
+  margin-top: 100px;
+
+  @media (max-width: 767px) {
+    margin-top: 50px;
+
+    & img {
+      width: 55px;
+    }
+  }
+`;
+
 export default function Partners() {
   return (
     <Section>
       <LogoPart />
-      <h1 style={{ fontWeight: "500", fontSize: "58px" }}>
-        We are in a good company
-      </h1>
-      <p style={{ width: "600px", fontSize: "20px", color: "#E5E5E5" }}>
+      <H1>We are in a good company</H1>
+      <Paragraph>
         Our partnerships have delivered great value to our projects and we’re
         happy to share some of their feedback below
-      </p>
-      <div
-        style={{
-          width: "10%",
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
+      </Paragraph>
+      <ArrowContainer>
         <img src="/assets/arrowLeft.png" alt="" />
         <img src="/assets/arrowRight.png" alt="" />
-      </div>
-      <p
-        style={{
-          width: "678px",
-          fontSize: "26px",
-          fontWeight: "normal",
-          color: "#E5E5E5",
-        }}
-      >
+      </ArrowContainer>
+      <Comment>
         Since 2019, Gravity team has been an astounding market maker for Bitkub.
         They have proven themselves to be one of the most consistent, committed
         and driven market makers on our exchange. Gravity Team has contributed
         high-quality volume and has proven to be very reliable and trustworthy
         partner. We strongly advocate Gravity Team as they have been an
         indispensable part of our market-making team.
-      </p>
-      <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-        Atthakrit Chimplapibul
-      </p>
-      <p
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
-          fontSize: "14px",
-        }}
-      >
+      </Comment>
+      <Name>Atthakrit Chimplapibul</Name>
+      <Grade>
         Co-founder & CEO of Bitkub |
         <img
           src="/assets/bitkub-logo-light-trimmed.png.png"
           alt=""
           style={{ marginLeft: "10px" }}
         />
-      </p>
-      <p style={{ marginTop: "100px" }}>
+      </Grade>
+      <Dots>
         <img src="/assets/dots.png" alt="" />
-      </p>
+      </Dots>
     </Section>
   );
 }
